806602dfc253578f21936aa6c1c8a120fc9c2474
Daniel Adam <daniel.adam1922@protonmail.com>
2021-08-16 17:56:44 +0200

    architecture: fix linter issues

---
+++ .mdl_style.rb
@@ -0,0 +1,8 @@
+all
+# something is broken about yaml front-matter detection
+exclude_rule 'MD002'
+exclude_rule 'MD041'
+# enable long lines
+exclude_rule 'MD013'
+# enable ordered lists indexed by letters (a, b, c)
+exclude_rule 'MD029'
